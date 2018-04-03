9191148522e9db03cbe26784782fa7336a9ae464
Sadeesha Eranga <msadeeshaeranga@gmail.com>
2018-04-03 23:49:36 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,7 +33,7 @@ $("#message-form").on('submit', function (e) {
         from: 'User',
         text: $('#txtMessage').val()
     }, function (data) {
-        // console.log(data);
+        $('#txtMessage').val('');
     });
 });
 
@@ -44,12 +44,16 @@ locationButton.on('click', function (e) {
         return alert('Geo location not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...')
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
